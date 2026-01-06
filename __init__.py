85bfbdb95b875875b9e9a9a8a4fc4b2bf8a1d720
saltchicken <john.eicher89@gmail.com>
2026-01-06 15:15:36 -0800

    Update init

--- __init__.py
+++ __init__.py
@@ -1,30 +1,40 @@
 from .load_image_from_path import LoadImageFromPath
 from .load_image_from_dir import LoadImageFromDir
 from .load_single_image_from_path import LoadSingleImageFromPath
-from .load_video_from_output import LoadVideoFromOutput, get_output_video_list
 
+from .load_video_from_output import LoadVideoFromOutput, get_video_output_dirs, get_videos_in_dir
 from .load_image_from_output import LoadImageFromOutput, get_output_dirs, get_images_in_dir
 from server import PromptServer
 from aiohttp import web
 
-@PromptServer.instance.routes.get("/my_custom_nodes/refresh_video_list")
-async def refresh_video_list(request):
-    files = get_output_video_list()
-    return web.json_response(files)
-
+# --- Image Routes ---
 
 @PromptServer.instance.routes.get("/my_custom_nodes/output_directories")
 async def get_output_directories(request):
     dirs = get_output_dirs()
     return web.json_response(dirs)
 
-
 @PromptServer.instance.routes.get("/my_custom_nodes/output_images")
 async def get_output_images(request):
-    # Get 'dir' from query parameters (default to empty string for root)
     target_dir = request.rel_url.query.get("dir", "")
     files = get_images_in_dir(target_dir)
     return web.json_response(files)
+
+
+
+
+@PromptServer.instance.routes.get("/my_custom_nodes/output_video_directories")
+async def get_output_video_directories(request):
+    dirs = get_video_output_dirs()
+    return web.json_response(dirs)
+
+
+@PromptServer.instance.routes.get("/my_custom_nodes/output_videos")
+async def get_output_videos(request):
+    target_dir = request.rel_url.query.get("dir", "")
+    files = get_videos_in_dir(target_dir)
+    return web.json_response(files)
+
 
 # Node Registration
 NODE_CLASS_MAPPINGS = {
